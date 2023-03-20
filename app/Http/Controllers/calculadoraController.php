23915840eaf6d75edf0887c916492766d1cbbdec
Franx21 <amvworld2015@gmail.com>
2023-03-20 04:18:09 -0700

    Se soluciono el problema de las variables en el input y el del

--- app/Http/Controllers/calculadoraController.php
+++ app/Http/Controllers/calculadoraController.php
@@ -8,29 +8,53 @@ use LDAP\Result;
 class calculadoraController extends Controller
 {
     //
-    public function calculate(Request $request){
-        $num1 = $request->number1;
-        $num2 = $request->number2;
-        $op = $request->operador;
-        if ($op == "+") {
-            $resultado = $num1 + $num2;
-            $resul = $resultado;
-            return view('welcome')->with('resul', $resul)->with('num1', $num1)->with('num2', $num2)->with('op', $op);
-        }elseif ($op == "-") {
-            $resultado = $num1 - $num2;
-            $resul = $resultado;
-            return view('welcome')->with('resul', $resul)->with('num1', $num1)->with('num2', $num2)->with('op', $op);
-        }elseif ($op == "*") {
-            $resultado = $num1 * $num2;
-            $resul = $resultado;
-            return view('welcome')->with('resul', $resul)->with('num1', $num1)->with('num2', $num2)->with('op', $op);
-        }elseif ($op == "/") {
-            $resultado = $num1 / $num2;
-            $resul = $resultado;
-            return view('welcome')->with('resul', $resul)->with('num1', $num1)->with('num2', $num2)->with('op', $op);
-        }else{
-            $resul = 0;
-            return view('welcome')->with('resul', $resul)->with('num1', $num1)->with('num2', $num2)->with('op', $op);
+    public function calculate(Request $request)
+    {
+        $_COOKIE_name1 = "num";
+        $_COOKIE_name2 = "op";
+        $_COOKIE_value1 = "";
+        $_COOKIE_value2 = "";
+
+        if (isset($_POST['num'])) {
+            $num = $_POST['input'] . $_POST['num'];
+            return view('welcome')->with('num', $num);
+        } else {
+            $num = "";
+        }
+        if (isset($_POST['op'])) {
+            $_COOKIE_value1 = $_POST['input'];
+            setcookie($_COOKIE_name1, $_COOKIE_value1, time() + (86400 * 30), "/");
+
+            $_COOKIE_value2 = $_POST['op'];
+            setcookie($_COOKIE_name2, $_COOKIE_value2, time() + (86400 * 30), "/");
+            $num = "";
+
+            return view('welcome')->with('op', $_COOKIE_value2);
+        }
+        if (isset($_POST['equal'])) {
+            $num = $_POST['input'];
+            $op = $_COOKIE['op'];
+            if ($op == "+") {
+                $resultado = $_COOKIE['num'] + $num;
+                $num = $resultado;
+                return view('welcome')->with('num', $num);
+            } elseif ($op == "-") {
+                $resultado = $_COOKIE['num'] - $num;
+                $num = $resultado;
+                return view('welcome')->with('num', $num);
+            } elseif ($op == "*") {
+                $resultado = $_COOKIE['num'] * $num;
+                $num = $resultado;
+                return view('welcome')->with('num', $num);
+            } elseif ($op == "/") {
+                $resultado = $_COOKIE['num'] / $num;
+                $num = $resultado;
+                return view('welcome')->with('num', $num);
+            }
+        }
+        if (isset($_POST['del'])) {
+            unset($resul);
+            return view('welcome');
         }
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-gradient-to-r from-sky-500 to-indigo-500">
     <form action="{{ route('calculadora.calculate') }}" method="POST">
         @csrf
         <div class="grid place-items-center h-screen">
@@ -17,53 +17,45 @@
                 <div class="grid grid-cols-4  text-white bg-white dark:bg-gray-800 dark:border-gray-700">
                     <div
                         class="grid grid-cols-3 grid-rows-2 text-black max-w-sm p-1 col-span-4 h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <input type="text" class="text-right" name="number1" id="number1" placeholder="numero 1">
-                        <input type="text" class="text-center" name="operador" id="operador" placeholder="operador">
-                        <input type="text" class="text-left" name="number2" id="number2" placeholder="numero 2">
-                        <input type="text" class="col-span-3 text-right" name="resultado" id="resultado"
-                            placeholder="@php
-                                echo @$num1." ".@$op." ".@$num2
-                            @endphp = @php
-                                echo @$resul
-                            @endphp">
+                        <input type="text" class="col-span-3 row-span-2 text-right" name="input" value="@php echo @$num @endphp">
                     </div>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">7</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">8</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">9</button>
+                    <input type="submit" value="7" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="8" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="9" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
                     <div
                         class="grid row-span-4 text-white bg-white border shadow dark:bg-gray-800 dark:border-gray-700">
-                        <button
-                            class="max-w-sm  bg-white hover:bg-red-700 border dark:border-red-700 dark:bg-red-800 dark:hover:bg-red-700">DEL</button>
-                        <button
-                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">/</button>
-                        <button
-                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">*</button>
-                        <button
-                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">-</button>
-                        <button
-                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">+</button>
+                        <input type="submit" value="del" name="del"
+                            class="max-w-sm  bg-white hover:bg-red-700 border dark:border-red-700 dark:bg-red-800 dark:hover:bg-red-700">
+                        <input type="submit" value="/" name="op"
+                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">
+                        <input type="submit" value="*" name="op"
+                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">
+                        <input type="submit" value="-" name="op"
+                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">
+                        <input type="submit" value="+" name="op"
+                            class="max-w-sm bg-white hover:bg-gray-600 border dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-grey-600">
                     </div>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">4</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">5</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">6</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">1</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">2</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">3</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">.</button>
-                    <button
-                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">0</button>
-                    <button type="submit"
-                        class="max-w-sm p-3 bg-white hover:bg-green-700 border dark:border-green-700 dark:bg-green-800 dark:hover:bg-green-700">=</button>
+                    <input type="submit" value="4" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="5" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="6" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="1" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="2" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="3" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="." name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="0" name="num"
+                        class="max-w-sm p-3 bg-white hover:bg-gray-700 border dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-grey-700">
+                    <input type="submit" value="=" name="equal"
+                        class="max-w-sm p-3 bg-white hover:bg-green-700 border dark:border-green-700 dark:bg-green-800 dark:hover:bg-green-700">
                 </div>
             </div>
         </div>
